8040c346e706dba31f9fb3fc0927692a2f73bfb4
Max Rugen <hi@maxrugen.com>
2025-12-30 11:34:04 +0100

    Add Vercel Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
+    import { injectAnalytics } from '@vercel/analytics/sveltekit';
     import '../app.css';
+    
+    injectAnalytics();
+
     if (typeof navigator !== 'undefined' && 'serviceWorker' in navigator) {
         window.addEventListener('load', () => {
             navigator.serviceWorker.register('/service-worker.js').catch(() => {});
